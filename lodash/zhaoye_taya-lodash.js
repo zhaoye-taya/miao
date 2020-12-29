49c72d67813f4cb0f6cadcb3bde626b0fcbdba31
zhaoye-taya <757668356@qq.com>
2020-12-29 11:20:44 +0800

    lodash2

--- lodash/zhaoye_taya-lodash.js
+++ lodash/zhaoye_taya-lodash.js
@@ -1,11 +1,11 @@
 var zhaoye_taya = {
-  chunk: function (ary, size) {
+    chunk: function(ary, size) {
     var result = []
     for (var i = 0; i < ary.length; i++) {
       
     }
   },
-  concat: function(ary) {
+  compact: function(ary) {
     var result = []
     for (var i = 0; i < ary.length; i++) {
       if(ary[i]) {
@@ -21,7 +21,55 @@ var zhaoye_taya = {
     }
     return result
   },
+  head: function(ary) {
+    return ary[0]
+  },
+  fill: function(ary, value, start, end) {
+    for (var i = 0; i < ary.length; i++){
+      
+    }
+  },
+  ary: function(f, n = f.length) {
+    return function(...args) {
+      return f(...args.slice(0,n))
+    }
+  },
+  before: function(n,func) {
+    var c = 0
+    var result
+    return function(...args) {
+      if(c < n) {
+        return result = func.call(this, ...args)
+        c++
+      }else {
+        return result
+      }
+    }
+  },
+  after: function(n,func) {
+    var c = 0
+    return function(...args) {
+      c++
+      if(c > n) {
+        return func.call(this, ...args) 
+      }
+    }
+  },
+  flip: function(func) {
+    return function (...args) {
+      return func(...args, reverse())
+    }
+  },
+  negata: function(predicate) {
+    return function(...args) {
+      return !predicate(...args)
+    }
+  },
+  spread: function(func) {
+    return function(ary) {
+      return func.apply(this, ary)
+    }
+    return func.f.bind(func, this)(ary)
+  },
   
-
-
 }
